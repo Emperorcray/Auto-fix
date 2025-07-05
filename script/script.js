4cdee243b5267661dfe2df4c96198792e86db2cf
Emperorcray <155745445+Emperorcray@users.noreply.github.com>
2025-07-05 15:50:23 +0100

    added a new breakpoint

--- script/script.js
+++ script/script.js
@@ -68,3 +68,10 @@ window.onclick = function(event) {
     }
   }
 }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+              
